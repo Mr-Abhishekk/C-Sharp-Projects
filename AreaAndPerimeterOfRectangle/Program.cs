b2fcf4336bcd1eda5b5860d9b1fcd3e0a99a3055
Mr-Abhishekk <munavalliabhishek1@gmail.com>
2026-02-19 15:27:45 +0530

    Added 3 C# Projects Clean

---
+++ AreaAndPerimeterOfRectangle/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rec = new Rectangle();
+            Console.WriteLine("Enter the Length of the Rectangle");
+            int l = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Width of the Rectangle");
+            int w = Convert.ToInt32(Console.ReadLine());
+            int area = rec.Area(l, w);
+            Console.WriteLine($"The Area of The Rectangle is {area}");
+            int peri = rec.Perimeter(l, w);
+            Console.WriteLine($"The Perimeter of the Rectangle is {peri}");
+
+        }
+    }
+    public class Rectangle
+    {
+        public int Area(int length, int width)
+        {
+            int area = length * width;
+            return area;
+        }
+        public int Perimeter(int x, int y)
+        {
+            int peri = 2 * (x + y);
+            return peri;
+        }
+   
+    }
+    }
+    
+    
+
---
+++ SumAndAverage/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace SumAndAverage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the First Number:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Second Number:");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Third Number:");
+            int z = Convert.ToInt32(Console.ReadLine());
+            Math math = new Math();
+            int sum = math.Sum(x, y, z);
+            Console.WriteLine($"The Sum of 3 Number is :{sum}");
+            float Avg = math.Average(x, y, z);
+            Console.WriteLine($"The Average of the 3 Number are:{Avg}");
+        }
+    }
+    public class Math
+    {
+        public int Sum(int x,int y,int z)
+        {
+            int sum = x + y + z;
+            return sum;
+        }
+
+        public float Average(int x,int y,int z)
+        {
+            float Avg = (x + y + z) / 3.0f;
+            return Avg;
+        }
+    }
+
+}
