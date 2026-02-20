12bb6f14c30a879813e9a9d31c838d0184364e47
Mr-Abhishekk <munavalliabhishek1@gmail.com>
2026-02-20 20:38:22 +0530

    Added new C# Project -Teacherdetails git push

---
+++ TeacherDetails/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace TeacherDetails
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Teacher teacher = new Teacher();
+            Console.WriteLine($"The Teacher name is {teacher.name}");
+            Console.WriteLine($"The Teacher is {teacher.age} years old");
+            Console.WriteLine($"The Teacher have experience of {teacher.exp} Years");
+            Console.WriteLine($"The Teacher subject  is {teacher.subject}");
+            teacher.TeacherDetails();
+            Console.ReadLine();
+        }
+    }
+    public class Teacher
+    {
+        public string name = "Bhavana";
+        public int age = 35;
+        public int exp = 7;
+        public string subject = "Computer Science";
+
+
+    public void TeacherDetails()
+        {
+
+        }
+
+    }
+}
